4e7c812b79b8cd305088b7ed2a1fc4057d577187
HAMDOUDI-Tarek <hamdouditarek@gmail.com>
2023-09-08 05:35:31 -0700

    readme

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * 
+ * main - Entry point
+ *
+ * Retrun: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char a;
+
+	for(a = 'a'; a <= 'z'; a++)
+	{
+		if (a != 'q')
+		{
+			if (a != 'e')
+				putchar(a);
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
+
