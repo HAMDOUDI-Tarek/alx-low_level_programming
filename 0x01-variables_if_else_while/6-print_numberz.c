08e25770597e85bae6ac0876a5dfeed7b42e633f
HAMDOUDI-Tarek <hamdouditarek@gmail.com>
2023-09-08 06:00:10 -0700

    readme

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a < 10; a++)
+	{
+		b = (char) a;
+		purchar(b);
+	}
+
+	putchar('\n');
+	return (0);
+}
