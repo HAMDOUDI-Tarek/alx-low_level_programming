9ff5e15235db52f855cb28d5e3c30a2526e105a3
HAMDOUDI-Tarek <hamdouditarek@gmail.com>
2023-09-12 10:46:35 -0700

    loops

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_to_98 - detects the letters.
+ * @n: the letter to start from.
+ * Return: Always 0.
+ */
+void print_to_98(int n)
+{
+	int c;
+	if (n > 0)
+	{
+		for (c = n; c <= 98; c++)
+		{
+			_putchar(c + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	else
+	{
+		for (c = n; c >= 98; c--)
+                {
+                        _putchar(c + '0');
+                        _putchar(',');
+                        _putchar(' ');
+                }
+}
