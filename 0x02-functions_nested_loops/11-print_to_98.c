cd9aa5b17b4b6d8f30d56874035bf796d2d626d7
HAMDOUDI-Tarek <hamdouditarek@gmail.com>
2023-09-12 11:12:40 -0700

    loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,16 +7,17 @@
  */
 void print_to_98(int n)
 {
-	int c;
+	int c, a = 98;
 
 	if (n <= 98)
 	{
-		for (c = n; c <= 98; c++)
+		for (c = n; c <= 97; c++)
 			printf("%d, ", c);
 	}
 	else
 	{
-		for (c = n; c >= 98; c--)
+		for (c = n; c >= 99; c--)
 			printf("%d, ", c);
 	}
+	printf("%d", a);
 }
