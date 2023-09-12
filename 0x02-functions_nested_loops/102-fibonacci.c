2f9c400a6a70274b5f3aaf9f5004e2770da00d4a
HAMDOUDI-Tarek <hamdouditarek@gmail.com>
2023-09-12 12:53:17 -0700

    loops

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 1, b = 2, c, d;
+
+	printf("%d, %d, ", a, b);
+	for (c = 1; c < 50; c++)
+	{
+	        d = a;
+	        a = b;
+	        b = a + d;
+	        printf("%d, ", b);
+	}
+	b = b + a;
+	printf("%d\n", b);
+}
