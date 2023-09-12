d12745006079af0cc33b55ccd564269b87748ce7
HAMDOUDI-Tarek <hamdouditarek@gmail.com>
2023-09-12 11:17:08 -0700

    loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,13 +1,31 @@
 #include "main.h"
 /**
- * _abs - detects the letters.
- * @c: the letter to test.
+ * times_table - detects the letters.
  * Return: Always 0.
  */
-int _abs(int c)
+void times_table(void)
 {
-	if (c < 0)
-		return (c * -1);
-	else
-		return (c);
+	int a, b, c;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar(48);
+		for (b = 1; b <= 9; b++)
+		{
+			c = a * b;
+			_putchar(44);
+			_putchar(32);
+			if (c <= 9)
+			{
+				_putchar(32);
+				_putchar(c + 48);
+			}
+			else
+			{
+				_putchar((c / 10) + 48);
+				_putchar((c % 10) + 48);
+			}
+		}
+		_putchar('\n');
+	}
 }
