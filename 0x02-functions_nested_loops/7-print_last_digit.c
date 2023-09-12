e584f3a6cec3de7dce01a73d227db5bdf27894d7
HAMDOUDI-Tarek <hamdouditarek@gmail.com>
2023-09-12 09:39:13 -0700

    loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,10 +1,12 @@
 #include "main.h"
 /**
- * _print_last_digit - prints last digit of given number.
+ * print_last_digit - prints last digit of given number.
  * @c: the letter to test.
  * Return: Always 0.
  */
 int print_last_digit(int c)
 {
-	return (c % 10);
+	if c < 0
+		c = c * -1
+	_putchar(c % 10 + '0');
 }
