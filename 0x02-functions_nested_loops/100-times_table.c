b8df7141a7960a7dce05d93a6f2f708e8ad81267
HAMDOUDI-Tarek <hamdouditarek@gmail.com>
2023-09-12 12:31:31 -0700

    loops

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/**
+ * print_times_table - detects the letters.
+ * @n: the number to contruct the time table from.
+ * Return: Always 0.
+ */
+void print_times_table(int n)
+{
+	int a, b, c;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (a = 0; a <= n; a++)
+		{
+			for (b = 0; b <= n; b++)
+			{
+				c = (a * b);
+				if (b == 0)
+					_putchar('0' + c);
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					if (c <= 9)
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar('0' + c);
+					}
+					else if (c > 9 && c < 100)
+					{
+						_putchar(' ');
+						_putchar('0' + (c / 10));
+						_putchar('0' + (c % 10));
+					}
+					else if (product >= 100)
+					{
+						_putchar('0' + (c / 100));
+						_putchar('0' + ((c / 10) % 10));
+						_putchar('0' + (c % 10));
+					}
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
