fbd5a78f6f1187a46b8fb5d6226c42dc4f270cb9
HAMDOUDI-Tarek <hamdouditarek@gmail.com>
2023-09-12 06:47:55 -0700

    loops

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isalpha - detects the lowercare letters.
+ * @c: the letter to test.
+ * Return: Always 0.
+ */
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122 || c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
