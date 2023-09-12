58310e6d6e839d8bec5efca3911a7f70cff2a052
HAMDOUDI-Tarek <hamdouditarek@gmail.com>
2023-09-12 09:51:49 -0700

    loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,8 +6,12 @@
  */
 int print_last_digit(int c)
 {
+	int s;
 	if (c < 0)
 		c = c * -1;
-	_putchar((c % 10) + '0');
-	return (c % 10);
+	s = c % 10;
+	if s < 0;
+		s = s * -1;
+	_putchar(s + '0');
+	return (s);
 }
