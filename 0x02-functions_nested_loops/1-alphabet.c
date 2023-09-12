5bc82879faa3f7563de983b9a1c886fe06fc9704
HAMDOUDI-Tarek <hamdouditarek@gmail.com>
2023-09-12 06:27:05 -0700

    loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * main - check the code for Holberton School students.
+ * print_alphabet - Print the alphabet.
  *
  * Return: Always 0.
  */
 void print_alphabet(void)
-{	
+{
 	int a;
 
 	for (a = 'a'; a <= 'z'; a++)
