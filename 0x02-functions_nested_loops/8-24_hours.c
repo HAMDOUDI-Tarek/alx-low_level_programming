23680603db0021ecf87036f353770da4893f0613
HAMDOUDI-Tarek <hamdouditarek@gmail.com>
2023-09-12 10:27:25 -0700

    loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,26 +1,25 @@
 #include "main.h"
 /**
  * jack_bauer - writes minutes of the day.
- * 
  * Return: Always 0.
  */
 void jack_bauer(void)
 {
 	int h, m;
-        h = 0;
-        while (h <= 23)
-        {
-                m = 0;
-                while (m < 60)
-                {
-                        _putchar((h / 10) + '0');
-                        _putchar((h % 10) + '0');
-                        _putchar(':');
-                        _putchar((m / 10) + '0');
-                        _putchar((m % 10) + '0');
-                        _putchar('\n');
-                        m++;
-                }
-                h++;
-        }
+	h = 0;
+	while (h <= 23)
+	{
+		m = 0;
+		while (m < 60)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+			m++;
+		}
+		h++;
+	}
 }
