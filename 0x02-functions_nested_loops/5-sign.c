59eaec715a231e4ac8433bf4f4cf2ca58a6bf37d
HAMDOUDI-Tarek <hamdouditarek@gmail.com>
2023-09-12 06:54:48 -0700

    loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - detects the sign.
+ * @c: the letter to test.
+ * Return: Always 0.
+ */
+int print_sign(int c)
+{
+	if (c < 0)
+	{
+		_putchar('-'):
+		return (-1);
+	}
+	else if (c == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('+');
+		return (1)
+	}
+}
