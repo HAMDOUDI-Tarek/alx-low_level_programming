828191f39dad3ea744737c5451b913046f1c5828
HAMDOUDI-Tarek <hamdouditarek@gmail.com>
2023-09-12 08:04:26 -0700

    loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs(int) - detects the letters.
+ * @c: the letter to test.
+ * Return: Always 0.
+ */
+int _abs(int c)
+{
+	if (c < 0)
+		return (n*-1);
+	else
+		return (n);
+}
