e3a3d2054db098c43625bc1b65304d54c5751be1
HAMDOUDI-Tarek <hamdouditarek@gmail.com>
2023-09-14 09:57:56 -0700

    loops

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -5,4 +5,14 @@
  */
 int main(void)
 {
-	
+	long int num = 612852475143;
+	long int div;
+
+	for (div = 2; div < num; div++)
+	{
+		if (num % div == 0)
+			num = num / div;
+	}
+	printf("%ld\n", div);
+	return (0);
+}
