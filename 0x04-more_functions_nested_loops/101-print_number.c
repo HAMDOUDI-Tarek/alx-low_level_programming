b511729b864e52b6e5b68334bc7bf71faf46dc87
HAMDOUDI-Tarek <hamdouditarek@gmail.com>
2023-09-14 09:46:25 -0700

    loops

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_number: display the given number.
+ * @n: number to display.
+ */
+int print_number(int n)
+{
+	unsigned int temp, div, count = 1;
+
+	if  (n < 0)
+	{
+		temp = n * -1;
+		_putchar('-');
+	}
+
+	else
+		temp = n;
+	div = temp;
+	while (div > 9)
+	{
+		div /= 10;
+		count *= 10;
+	}
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((temp / count) % 10) + 48);
+	}
+}
