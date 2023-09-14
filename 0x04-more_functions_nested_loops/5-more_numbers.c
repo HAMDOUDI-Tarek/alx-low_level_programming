b843c8b40ba253eb4f35eb094353f786b120177c
HAMDOUDI-Tarek <hamdouditarek@gmail.com>
2023-09-14 04:09:15 -0700

    more loop

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,14 +5,15 @@
  */
 void more_numbers(void)
 {
-	int n, c;
-	
+	int n;
+	int c;
+
 	for (c = 0; c < 10; c++)
 	{
 		n = 0;
 		while (n <= 14)
 		{
-			if (i >= 10)
+			if (n >= 10)
 			{
 				_putchar(n / 10 + '0');
 			}
