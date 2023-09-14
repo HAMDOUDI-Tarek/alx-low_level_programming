7d02f023335c223decff87bcc258f4e98d4a8fb4
HAMDOUDI-Tarek <hamdouditarek@gmail.com>
2023-09-14 10:32:07 -0700

    loops

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
-  * main - Test function for positive or negative
-  * Return: 0
+  * main - Entry point
+  * Return: Always 0.
   */
 
 int main(void)
 {
-	int number;
+	int i;
 
 	number = 0;
-	positive_or_negative(number);
+	positive_or_negative(i);
 
 return (0);
 }
