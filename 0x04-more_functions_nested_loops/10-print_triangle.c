b812ab4c796fdd43e5378669cec35b65c572f077
HAMDOUDI-Tarek <hamdouditarek@gmail.com>
2023-09-14 08:10:36 -0700

    loops

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point.
+ * Return: Always 0.
+ */
+void print_triangle(int size);
+{
+	int l, f;
+
+	for (l = 1; l <= size; l++)
+	{
+		f = 0;
+		while (f < (size - l))
+		{
+			_putchar(' ');
+			f++;
+		}
+		for (f = (size - l ); f < size; f++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
