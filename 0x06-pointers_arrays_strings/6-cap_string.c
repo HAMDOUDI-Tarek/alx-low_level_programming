912f9276ed9b6fc8fc10a17988e0825a9b8da0ad
HAMDOUDI-Tarek <hamdouditarek@gmail.com>
2023-09-20 13:23:14 -0700

    everything

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,7 +8,7 @@
 char *cap_string(char *s)
 {
 	int i, counter = 0;
-	int sep[] = {9, 10, 32, 33, 34, 40, 41, 44, 46, 59, 63, 123, 125};	/*list was sorted using python*/
+	int sep[] = {9, 10, 32, 33, 34, 40, 41, 44, 46, 59, 63, 123, 125};
 
 	if (*(s + counter) >= 97 && *(s + counter) <= 122)
 		*(s + counter) = *(s + counter) - 32;
