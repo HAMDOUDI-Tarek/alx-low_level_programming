932d9753c4d77893c93b76c84e455aff2096d29f
HAMDOUDI-Tarek <hamdouditarek@gmail.com>
2023-09-20 06:33:55 -0700

    pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * _strcat - concatenates two strings.
+ * @dest: destination.
+ * @src: source.
+ * Return: destination.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int counter1, counter2;
+	counter1 = counter2 = 0;
+
+	while (*(dest + counter1) != '\0')
+	{
+		counter1++;
+	}
+
+	while (counter2 >= 0)
+	{
+		*(dest + counter1) = *(src + counter2);
+		if (*(src + counter2) == '\0')
+			break;
+		counter1++;
+		counter2++;
+	}
+	return (dest);
+}
