b9b2ddd191ef20d3b918eba8f95447cba5aa9558
HAMDOUDI-Tarek <hamdouditarek@gmail.com>
2023-09-20 07:15:32 -0700

    pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strncat - concatenate 2 strings.
+ * @dest: destination.
+ * @src: source.
+ * @n: byte number.
+ * Return: destination.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int counter1, counter2;
+	counter1 = counter2 = 0;
+
+	while (*(dest + counter1) != '\0')
+	{
+		counter1++;
+	}
+
+	while (counter2 < n)
+	{
+		*(dest + counter1) = *(src + counter2);
+		if (*(src + counter2) == '\0')
+			break;
+		counter1++;
+		counter2++;
+	}
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * leet - encodes a string into 1337.
+ * @s: input string.
+ * Return: destination
+ */
+
+char *leet(char *s)
+{
+	int i, counter = 0;
+	int lower[] = {97, 101, 111, 116, 108};
+	int upper[] = {65, 69, 79, 84, 76};
+	int nums[] = {52, 51, 48, 55, 49};
+
+	while (*(s + counter) != '\0')
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (*(s + counter) == lower[i] || *(s + count) == upper[i])
+			{
+				*(s + counter) = nums[i];
+				break;
+			}
+		}
+		counter++;
+	}
