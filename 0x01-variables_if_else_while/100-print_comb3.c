9a26aa95815c8ed7b49b4987fbf0e27df0ddccaf
HAMDOUDI-Tarek <hamdouditarek@gmail.com>
2023-09-23 14:40:12 -0700

    try

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,25 +6,25 @@
  */
 int main(void)
 {
-        int a, b;
+	int a, b;
 
-        for (a = 0; a <= 9; a++)
-        {
-                for (b = 0; b <= 9; b++)
-                {
-                        if (b > a && b != a)
-                        {
-                                putchar(a + '0');
-                                putchar(b + '0');
-                                if (a != 8 || b != 9)
-                                {
-                                        putchar(' ');
-                                        putchar(',');
-                                }
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			if (b > a && b != a)
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+				if (a != 8 || b != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 
-                        }
-                }
-        }
-        putchar('\n');
+			}
+		}
+	}
+	putchar('\n');
 	return (0);
 }
