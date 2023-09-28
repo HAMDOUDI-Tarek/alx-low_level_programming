324e548aecd3cd580e8b524ce97e9f654c2a4d74
HAMDOUDI-Tarek <hamdouditarek@gmail.com>
2023-09-28 11:42:15 -0700

    everything

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * is_prime_number - returns 1 if the input integer is a prime number, otherwise return 0.
+ * @n: number to evaluate.
+ * Return: Result.
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (helper(n, n - 1));
+}
+
+/**
+ * helper - the helper function.
+ * @n: number to evaluate
+ * @i: concatenated number.
+ * Return: 1 if n is prime, 0 if not.
+ */
+int helper(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (helper(n, i - 1));
+}
---
+++ 0x08-recursion/pri.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int helper(int i, int n);
+
+int is_prime_number(int n)
+{
+	printf("%d\n", helper(2,n));
+	if (helper(2, n) == n - 3)
+		return (0);
+	return (1);
+}
+
+int helper(int i, int n)
+{
+	if (n == 2)
+		return (1);
+	if (n <= 1 || i == n - 1 || n % i == 0)
+		return (0);
+	return 1 + helper(i + 1, n);
+}
+
+int main()
+{
+	int a;
+	printf("Number: ");
+	scanf("%d", &a);
+	printf("Result: %d\n", is_prime_number(a));
+}
