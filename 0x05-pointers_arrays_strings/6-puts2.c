76093c34680a7c40ceafa7863d2e2c6b1a89639d
HAMDOUDI-Tarek <hamdouditarek@gmail.com>
2023-10-03 06:44:31 -0700

    try

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,14 +7,14 @@ void puts2(char *str)
 {
         int i = 0, count = 0;
 
-        while (*(s + count) != '\0')
+        while (*(str + count) != '\0')
         {
                 count++;
         }
 
         while (i != count)
         {
-                putchar(s[i]);
+                putchar(str[i]);
                 i += 2;
         }
         putchar('\n');
