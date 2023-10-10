760f60fd233f6e53e7f6a99c4fc901886936440a
HAMDOUDI-Tarek <hamdouditarek@gmail.com>
2023-10-10 13:52:00 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
+#ifndef DOG_H
 #define DOG_H
-
 
 /**
  * struct dog - a dog's basic info
@@ -28,6 +28,4 @@ void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 
-
 #endif
-
