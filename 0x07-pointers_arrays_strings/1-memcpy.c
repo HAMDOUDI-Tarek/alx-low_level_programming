0ccd83e252258f0983478610672c376171f2f1f5
HAMDOUDI-Tarek <hamdouditarek@gmail.com>
2023-10-11 12:21:04 -0700

    everything

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,13 +10,12 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 
-	unsigned int size = n;
 
-	if (size > 0)
+	if (n > 0)
 	{
 		unsigned int i = 0;
 
-		while (i < size)
+		while (i < n)
 		{
 			dest[i] = src[i];
 			i++;
